86b43843185433e08e3bbaff2f6b84920db14ba9
choyeonjung134 <choyeonjung134@gmail.com>
2026-09-17 14:57:35 +0900

    카메라 추가

--- 1B_3Dyeon/Assets/Script/PlayerController.cs
+++ 1B_3Dyeon/Assets/Script/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpPower = 5f;
     public float gravity = -20f;
+    public float mouseSensitivity = 0.2f;
+
+    private Vector2 lookInput;
+
 
     private Vector2 moveInput;
     private float verticalVelocity;
@@ -28,8 +32,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+
     void Update()
     {
+        transform.Rotate(0f, lookInput.x * mouseSensitivity, 0f);
+
+        if (controller.isGrounded && verticalVelocity < 0f)
+        {
+            verticalVelocity = -2f;
+        }
+
+
         if (controller.isGrounded && verticalVelocity < 0f)
         {
             verticalVelocity = -2f;
@@ -37,10 +55,12 @@ public class PlayerController : MonoBehaviour
 
         verticalVelocity += gravity * Time.deltaTime;
 
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
         move = move * moveSpeed;
         move.y = verticalVelocity;
 
         controller.Move(move * Time.deltaTime);
+
+
     }
 }
